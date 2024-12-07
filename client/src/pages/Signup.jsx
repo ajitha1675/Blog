cf17345469c83f1b310f7502dd72024de5efb25b
Ajilalitha <ajilalitha010@gmail.com>
2024-12-07 16:43:44 +0530

    edit the singup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className=''>
+      {/* left */}
+    <div className=''>
+      <Link
+          to='/'
+          className='font-bold dark:text-white text-4xl'
+        >
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            Aji's
+          </span>
+          blog
+        </Link>
+        <p className=''>
+            This is a demo project. You can sign up with
+            your email and password or with Google.
+        </p>
+    </div>
+      {/* right */}
+    <div className=''>
+
+    </div>
+    </div>
   )
 }
 
